a30c03c2f2876691d1050ac89cbcd77b0e58df65
vaibhav-arde <vaibhav.arde@gmail.com>
2019-01-06 07:09:53 +0530

    JS datatypes and variables

---
+++ V02-DataTypesAndVariables.js
@@ -0,0 +1,15 @@
+/* Data Types in JS:
+    undefined, null, boolean, string, symbol, number and object
+*/
+
+//3 types of variable in JS( var, let, const)
+
+var myName = "VaibhaV";
+
+myName = "changedName";
+
+let anyName = "TestName";
+
+const pi =3.14;
+pi= "yt"; // this gives error while runtime .. TypeError: Assignment to constant variable.
+
