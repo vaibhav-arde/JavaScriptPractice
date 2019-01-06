614071076dee3c892830e3b1d8173cd6904ccade
vaibhav-arde <vaibhav.arde@gmail.com>
2019-01-06 09:09:35 +0530

    change in V03 and V04

--- V03-AssignmentOperator.js
+++ V03-AssignmentOperator.js
@@ -8,3 +8,14 @@ a= 7;
 console.log(a);
 b=a;
 console.log(b);
+
+let x= 7;
+let y = 8;
+let z = "I am a ";
+
+x = x + 1;
+y = y + 2;
+z = z + "String";
+console.log(x);
+console.log(y);
+console.log(z);
---
+++ V04-StringOperations.js
@@ -0,0 +1,18 @@
+let myName = "VaibhaV";
+
+console.log(myName.length);
+
+console.log(myName[0]); //V
+myName[0] ='B';// Strings are Immutable
+console.log(myName[0]);
+
+//Secondlast letter of myName
+console.log(myName[myName.length-2]);
+
+function fullName(firstName, middleName, lastName){
+    let result ="";
+    result += `My full name is "${firstName} ${middleName} ${lastName}".`
+    return result;
+}
+
+console.log(fullName("VaibhaV", "Bandopant", "Arde"));
