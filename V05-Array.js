940dac0f95f8c012b1e6c35ddb6052371b226f5a
vaibhav-arde <vaibhav.arde@gmail.com>
2019-01-19 04:07:58 +0530

    V05 Array

---
+++ V05-Array.js
@@ -0,0 +1,7 @@
+var sampleArray = ["VaibhaV", "Chandu", "Prasad"];
+
+console.log(sampleArray);
+
+//Nested array
+var nestedArray = [["Vaibhav", "Wipro"], ["Chandu", "EcoTech"], ["Prasad"]];
+console.log(nestedArray);
