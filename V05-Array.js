3df56afd943aad0b93ab764190f9e47c354eb811
vaibhav-arde <vaibhav.arde@gmail.com>
2019-01-19 04:17:57 +0530

    V05 Nested array

--- V05-Array.js
+++ V05-Array.js
@@ -3,5 +3,15 @@ var sampleArray = ["VaibhaV", "Chandu", "Prasad"];
 console.log(sampleArray);
 
 //Nested array
-var nestedArray = [["Vaibhav", "Wipro"], ["Chandu", "EcoTech"], ["Prasad"]];
+var nestedArray = [["VaibhaV", "Wipro"], ["Chandu", "EcoTech"], ["Prasad"]];
 console.log(nestedArray);
+
+//Array value using indices
+var valueByIndices = sampleArray[0];
+console.log(valueByIndices);
+
+var nestedByIndices = nestedArray[0];
+console.log(nestedByIndices);
+console.log(nestedByIndices[0]);
+// var nestedByIndices = nestedArray[0];
+// console.log(nestedByIndices);
