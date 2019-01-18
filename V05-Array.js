8bed292cc1ae3930ac3b4f47a1c87127a4b835aa
vaibhav-arde <vaibhav.arde@gmail.com>
2019-01-19 04:40:43 +0530

    V05 array.push()

--- V05-Array.js
+++ V05-Array.js
@@ -15,9 +15,16 @@ console.log(nestedByIndices);
 console.log(nestedByIndices[0]);
 
 //Modify array values
-nestedArray[2]="updated Prasad";
+//nestedArray[2]="updated Prasad";
 console.log(nestedArray);
 
 //Get vale form Multidimentional array
 var companyChandu= nestedArray[1][1];
 console.log(companyChandu);
+
+//Add values or array to an array using .push ()
+nestedArray.push("Sheetal", "TCS");
+nestedArray.push(["Sheetal", "TCS"]);
+console.log(nestedArray);
+nestedArray[2].push("XYZ");
+console.log(nestedArray);
